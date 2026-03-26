2920dd0f50220bbd3632f78107d5dd098addca8e
andradeje86-collab <andradeje86@gmail.com>
2026-03-26 00:20:11 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "COLE_SUA_API_KEY_AQUI",
+  authDomain: "COLE_SEU_AUTH_DOMAIN",
+  projectId: "COLE_SEU_PROJECT_ID",
+  storageBucket: "COLE_SEU_STORAGE_BUCKET",
+  messagingSenderId: "COLE_SEU_MESSAGING_ID",
+  appId: "COLE_SEU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
