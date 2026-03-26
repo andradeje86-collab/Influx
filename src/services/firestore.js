de8e5ffbcda716ee0b70737ad0096ef39099efa3
andradeje86-collab <andradeje86@gmail.com>
2026-03-26 00:48:46 -0300

    Update firestore.js

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,17 +1,1 @@
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA0RprYSHqAZwtdV7AoWV8r3tFQke8AK2o",
-  authDomain: "in-flux.firebaseapp.com",
-  projectId: "in-flux",
-  storageBucket: "in-flux.firebasestorage.app",
-  messagingSenderId: "1084464047425",
-  appId: "1:1084464047425:web:36ea7a064f0e994ad8adc0"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
