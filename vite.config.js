0e4dd9bd20590d903c0934fc96f0fbc30bd03934
andradeje86-collab <andradeje86@gmail.com>
2026-03-26 19:34:18 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Define o caminho base como o nome do seu repositório no GitHub
-  base: '/Influx/', 
-  
-  build: {
-    // Garante que o build seja gerado na pasta dist
-    outDir: 'dist',
-    // Limpa a pasta antes de gerar um novo build
-    emptyOutDir: true,
-  },
-  
-  server: {
-    // Configuração opcional para facilitar o desenvolvimento local
-    port: 3000,
-    open: true
-  }
+  plugins: [react()],
+  base: '/Influx/'
 })
