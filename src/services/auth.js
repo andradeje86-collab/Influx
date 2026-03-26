481cff1a7aafc564c9ac6fdd62e93c21a55594eb
andradeje86-collab <andradeje86@gmail.com>
2026-03-26 00:25:55 -0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,1 +1,27 @@
+import { auth } from "./firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+  onAuthStateChanged
+} from "firebase/auth";
 
+// Login
+export const login = (email, senha) =>
+  signInWithEmailAndPassword(auth, email, senha);
+
+// Cadastro
+export const cadastrar = (email, senha) =>
+  createUserWithEmailAndPassword(auth, email, senha);
+
+// Recuperar senha
+export const recuperarSenha = (email) =>
+  sendPasswordResetEmail(auth, email);
+
+// Logout
+export const logout = () => signOut(auth);
+
+// Escuta mudanças de login
+export const escutarAuth = (callback) =>
+  onAuthStateChanged(auth, callback);
